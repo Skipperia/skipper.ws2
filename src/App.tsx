d37f322eafb8c247e16ed12ae67e5ca85870e5a8
Alex Kipper <kippera27@gmail.com>
2024-06-20 22:05:46 +0300

    startup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.module.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <p>
+          lets gooo
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
